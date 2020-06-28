4ea43a7431e61f2c54fd2ed8a3f55353752f5d5e
Robson <robsonunifap@gmail.com>
2020-06-28 16:27:52 -0300

    Criando SplashActivity.kt

--- app/src/main/java/com/arp/arpvocal/Responsavel.kt
+++ app/src/main/java/com/arp/arpvocal/Responsavel.kt
@@ -3,7 +3,7 @@ package com.arp.arpvocal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class responsavel : AppCompatActivity() {
+class Responsavel : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_responsavel)
--- app/src/main/java/com/arp/arpvocal/SplashActivity.kt
+++ app/src/main/java/com/arp/arpvocal/SplashActivity.kt
@@ -1,29 +1,35 @@
 package com.arp.arpvocal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
+class SplashActivity : AppCompatActivity() {
+
+    // This is the loading time of the splash screen
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
+/* // This is original code on create
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
     }
 
-    /*
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable () {
-            @Override
-            public void run() {
-                mostrarMainActivity();
-            }
-        }, 2000);
-    }
-
-     */
-}
+}*/
